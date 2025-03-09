49f3d7bbd86244b8871ce295afbe24fe6bc43a98
Joe Kehnast <joe.kehnast@verizon.net>
2025-03-09 11:22:18 -0400

    initial checkin getting project to run

--- dist/app.js
+++ dist/app.js
@@ -2,5 +2,5 @@ function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Game started yay';
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './app/app.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js'
-  },
-  devServer: {
-    inline: false
-  }
+    entry: ['./app/app.ts'],
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '/index.html'
+        })
+    ],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: './dist/app.js',
+    },
+    devServer: {
+        watchFiles: {
+            paths: ['app/**/*']
+        }
+    }
 };
